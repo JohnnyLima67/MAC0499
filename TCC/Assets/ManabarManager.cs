430d9f06213a0f36c6a7e041c7ad9630e1a8dfc4
lincolnyuji <lincolnyuji@usp.br>
2023-04-07 16:44:03 -0300

    Add HUD with Health and Mana Bars
    - Two new HUD bars created: Health and Mana;
    - There's one manager for each bar which contains functions that increase or decrease the bar value;

---
+++ TCC/Assets/ManabarManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManabarManager : MonoBehaviour
+{
+    public Image manaBar;
+    public float maxMana = 200f;
+    public float mana = 40f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mana = Mathf.Clamp(mana, 0, maxMana);
+        manaBar.fillAmount = mana / maxMana;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            LoseMana(20);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetMana(10);
+        }
+    }
+
+    public void LoseMana(float amount)
+    {
+        if (amount > 0)
+        {
+            mana = Mathf.Max(mana - amount, 0);
+            manaBar.fillAmount = mana / maxMana;
+        }
+    }
+
+    public void GetMana(float amount)
+    {
+        if (amount > 0)
+        {
+            mana = Mathf.Clamp(mana + amount, 0, maxMana);
+            manaBar.fillAmount = mana / maxMana;
+        }
+    }
+}
