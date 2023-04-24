052b59d5152d85a21a39c34ec14f3141c29f8d89
JohnnyLima67 <64144848+JohnnyLima67@users.noreply.github.com>
2023-04-24 13:17:05 -0300

    Add Camera Follow Player

--- TCC/Assets/Scripts/CameraFollowPlayer.cs
+++ TCC/Assets/Scripts/CameraFollowPlayer.cs
@@ -9,20 +9,25 @@ public class CameraFollowPlayer : MonoBehaviour
     [SerializeField] public CinemachineVirtualCamera virtualCamera;  // The Cinemachine virtual camera component
 
     private IPlayerController _playerController;
+    private float fallTime = 0f;
 
     private void Awake() => _player.TryGetComponent(out _playerController);
 
     private void LateUpdate() {
-        float projectedPos = 0;
+        float projectedPosx = 0;
+        float projectedPosy = 0;
         //CinemachineComponentBase transposer = virtualCamera.GetComponentInChildren<CinemachineFramingTransposer>();
         CinemachineFramingTransposer transposer = virtualCamera.GetCinemachineComponent<CinemachineFramingTransposer>();
-
         if (_playerController != null) {
-            projectedPos = (_playerController.Velocity.normalized).x;
+            projectedPosx = (_playerController.Velocity.normalized).x;
+            if ((_playerController.Velocity).y < 0) projectedPosy = 0.6f; 
             // _velOffset = Vector3.SmoothDamp(_velOffset, projectedPos, ref _lookAheadVel, _lookAheadSpeed);
         }
         
-        if (projectedPos!=0) transposer.m_TrackedObjectOffset.x = projectedPos;
+        if (projectedPosx!=0) transposer.m_TrackedObjectOffset.x = projectedPosx;
+        if (_playerController.Velocity.y < -20) transposer.m_YDamping = projectedPosy;
+        else transposer.m_YDamping = 1;
+        Debug.Log(projectedPosy);
 
         //Step(_smoothTime);
     }
