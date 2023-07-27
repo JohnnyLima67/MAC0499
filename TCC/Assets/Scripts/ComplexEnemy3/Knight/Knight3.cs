d6c7d97ad459f45c31ec1a7a33ec7fdf0fc9d250
André Gustavo Nakagomi Lopez <andregnl@usp.br>
2023-07-27 17:00:41 -0300

    Add complex test enemy with fsm
    - Add sprite to test enemy
    
    Signed-off-by: André Gustavo Nakagomi Lopez <andregnl@usp.br>

--- TCC/Assets/Scripts/ComplexEnemy3/Knight/Knight3.cs
+++ TCC/Assets/Scripts/ComplexEnemy3/Knight/Knight3.cs
@@ -6,31 +6,89 @@ using FSM;
 public class Knight3 : MonoBehaviour
 {
     private StateMachine fsm;
+    private Transform lastPlayerPos = null;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] Enemy thisEnemy;
+    [SerializeField] Transform visionObject;
     [SerializeField] float detectionDistance = 20.0f;
+    [SerializeField] float speed = 2.0f;
+    [SerializeField] LayerMask playerAndGroundLayer;
 
     void Start()
     {
         fsm = new StateMachine(this);
 
-        fsm.AddState("Idle", onLogic: (state) => Debug.Log("Estou no Idle"));
-        fsm.AddState("Active", onLogic: (state) => Debug.Log("Estou no Active"));
+        fsm.AddState("Idle");
+        fsm.AddState("Active",
+                     onEnter: (state) => Debug.Log("Estou no Active"),
+                     onLogic: (state) => FollowPlayer());
+        fsm.AddState("Flip", onEnter: (state) => thisEnemy.Flip());
 
-        fsm.AddTwoWayTransition("Idle",
-                                "Active",
-                                t => DistanceToPlayer() < detectionDistance);
+        fsm.AddTransition("Idle",
+                          "Active",
+                          t => SawOrHeardPlayer());
+
+        fsm.AddTransition("Active", "Flip", t => IsInPlayerLastPos());
+        fsm.AddTransition("Flip", "Idle", t => true);
 
         fsm.SetStartState("Idle");
         fsm.Init();
     }
 
-    float DistanceToPlayer()
+    bool IsInPlayerLastPos()
     {
-        Vector3 playerPos = GameManager.Instance.playerObject.transform.position;
-        return Vector3.Distance(playerPos, transform.position);
+        if (Mathf.Abs(transform.position.x - lastPlayerPos.position.x) < 0.5) return true;
+        else return false;
     }
 
-    void Update()
+    bool SawOrHeardPlayer()
+    {
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position,
+                                             Vector3.Normalize(visionObject.transform.position - transform.position),
+                                             playerAndGroundLayer);
+        Debug.DrawRay(transform.position,
+                      Vector3.Normalize(visionObject.transform.position - transform.position), Color.red);
+        if (hit.collider != null)
+        {
+            Debug.Log(hit.collider.gameObject);
+            if (hit.collider.CompareTag("Player"))
+            {
+                lastPlayerPos = hit.collider.gameObject.transform;
+                return true;
+            }
+        }
+
+        lastPlayerPos = null;
+        return false;
+    }
+
+    void FollowPlayer()
+    {
+        Vector3 direction = Vector3.Normalize(lastPlayerPos.transform.position - transform.position);
+        Vector3 s = direction * speed;
+        s.y = rb.velocity.y;
+
+        rb.velocity = s;
+    }
+
+    float Step()
+    {
+        return speed * Time.deltaTime;
+    }
+
+    void FixedUpdate()
     {
         fsm.OnLogic();
     }
+
+    // void OnDrawGizmos()
+    // {
+    //     if(transform.position != null && visionObject != null)
+    //      {
+    //          // Draws a blue line from this transform to the target
+    //          Gizmos.color = Color.magenta;
+    //          Gizmos.DrawLine(transform.position, visionObject.transform.position);
+    //      }
+    // }
 }
---
+++ TCC/Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private bool isFlipped = false;
+    [SerializeField] GameObject flippableAssets;
+
+    public void Flip()
+    {
+        Quaternion newRotation = flippableAssets.transform.rotation;
+
+        if (isFlipped)
+        {
+            newRotation.y = 0.0f;
+            isFlipped = false;
+        }
+        else
+        {
+            newRotation.y = -180.0f;
+            isFlipped = true;
+        }
+
+        flippableAssets.transform.rotation = newRotation;
+    }
+}
