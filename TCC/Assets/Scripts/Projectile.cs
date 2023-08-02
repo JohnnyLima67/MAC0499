c94bd4413875dc0794f510e6d07e98c79a9d0138
JohnnyLima67 <64144848+JohnnyLima67@users.noreply.github.com>
2023-08-02 18:00:29 -0300

    Add bugs to flecha

--- TCC/Assets/Scripts/Projectile.cs
+++ TCC/Assets/Scripts/Projectile.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Projectile : MonoBehaviour
 {
     [SerializeField] float damage;
@@ -9,35 +10,53 @@ public class Projectile : MonoBehaviour
     [SerializeField] Rigidbody2D rigidbody;
     [SerializeField] float projectileDuration;
 
+	public Vector3 velocity;
+	public LayerMask hitMask = -1; // all layers by default
+
+
     float time = 0.0f;
 
     public void FireProjectile(Vector3 direction)
     {
-        rigidbody.velocity = direction * speed;
+        //rigidbody.velocity = direction * speed;
+        return;
     }
 
-    public void FixedUpdate()
+    public void Update()
     {
         time += Time.deltaTime;
 
         if (time >= projectileDuration) ExpireSequence();
+
+        		// get difference between last position and next position
+		Vector3 displacement = velocity * Time.fixedDeltaTime;
+
+		// get Raycast ray (cache last position)
+		Ray2D ray = new Ray2D(transform.position, displacement);
+		RaycastHit2D hit;
+
+		// apply displacement
+		transform.position += displacement;
+        
+
+		// raycast for any collisions since last position
+        hit = Physics2D.Raycast(transform.position, displacement, 2, hitMask);
+        Debug.DrawRay(transform.position, displacement);
+
+		if (hit.collider != null)
+		{
+			// hit something!
+			
+			// example events
+			Debug.Log(this.name + " has hit " + hit.collider.name + " at point " + hit.point);
+			//hit.collider.SendMessage("OnProjectileCollision", this, SendMessageOptions.DontRequireReceiver);
+			Destroy(this.gameObject);
+		}
+
+
     }
 
-    void OnTriggerEnter2D(Collider2D col)
-    {
-        if (col.CompareTag("Player") || col.CompareTag("Camera") || col.CompareTag("PlayerProjectile")) return;
 
-        HittableBehaviour h = col.gameObject.GetComponent<HittableBehaviour>();
-        if (h != null)
-        {
-            h.TakeDamage(damage);
-            HitHittableSequence();
-        }
-        else
-        {
-            HitNonHittableSequence();
-        }
-    }
 
     void HitHittableSequence()
     {
@@ -54,3 +73,37 @@ public class Projectile : MonoBehaviour
         Destroy(gameObject);
     }
 }
+
+/*
+public class Projectile : MonoBehaviour
+{
+	public Vector3 velocity;
+	public LayerMask hitMask = -1; // all layers by default
+
+	void FixedUpdate()
+	{
+
+		// get difference between last position and next position
+		Vector3 displacement = velocity * Time.fixedDeltaTime;
+
+		// get Raycast ray (cache last position)
+		Ray ray = new Ray(transform.position, displacement);
+		RaycastHit hit;
+
+		// apply displacement
+		transform.position += displacement;
+
+		// raycast for any collisions since last position
+		if (Physics.Raycast(ray, out hit, displacement.magnitude, hitMask))
+		{
+			// hit something!
+			
+			// example events
+			Debug.Log(this.name + " has hit " + hit.collider.name + " at point " + hit.point);
+			hit.collider.SendMessage("OnProjectileCollision", this, SendMessageOptions.DontRequireReceiver);
+			Destroy(this.gameObject);
+		}
+	}
+}
+
+*/
