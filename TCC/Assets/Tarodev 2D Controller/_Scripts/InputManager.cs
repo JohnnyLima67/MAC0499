8c7904962efc67a2e6ca838ad8c41944e9e9ac57
JohnnyLima67 <64144848+JohnnyLima67@users.noreply.github.com>
2023-08-23 13:23:42 -0300

    Add range attack key to gamepad menu
    Also fixed input error related to "attack2".

--- TCC/Assets/Tarodev 2D Controller/_Scripts/InputManager.cs
+++ TCC/Assets/Tarodev 2D Controller/_Scripts/InputManager.cs
@@ -11,7 +11,7 @@ namespace TarodevController {
 
 
         private PlayerInput _playerInput;
-        private InputAction _move, _jump, _dash, _attack, _attack2, _rangedAttack, _powerUp,_exampleAction, _escape;
+        private InputAction _move, _jump, _dash, _attack, _rangedAttack, _powerUp,_exampleAction, _escape;
 
         private void Awake() {
 
@@ -28,7 +28,6 @@ namespace TarodevController {
             _jump = _playerInput.actions["Jump"];
             _dash = _playerInput.actions["Dash"];
             _attack = _playerInput.actions["Attack"];
-            _attack2 = _playerInput.actions["Attack2"];
             _powerUp = _playerInput.actions["PowerUp"];
             _rangedAttack = _playerInput.actions["RangedAttack"];
             _escape = _playerInput.actions["MenuOpenClose"];
@@ -45,7 +44,6 @@ namespace TarodevController {
                 JumpHeld = _jump.IsPressed(),
                 DashDown = _dash.WasPressedThisFrame(),
                 AttackDown = _attack.WasPressedThisFrame(),
-                Attack2Down = _attack2.WasPressedThisFrame(),
                 PowerUp = _powerUp.WasPressedThisFrame(),
                 RangedAttackDown = _rangedAttack.WasPressedThisFrame(),
                 EscapeDown = _escape.WasPressedThisFrame(),
@@ -59,7 +57,6 @@ namespace TarodevController {
         public bool JumpHeld;
         public bool DashDown;
         public bool AttackDown;
-        public bool Attack2Down;
         public bool PowerUp;
         public bool RangedAttackDown;
         public bool EscapeDown;
