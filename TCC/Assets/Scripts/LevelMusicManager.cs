4628374334148d9810c5786e361dabfacbc18dd7
André Gustavo Nakagomi Lopez <andregnl@usp.br>
2023-11-03 15:56:23 -0300

    Add music fade in to LevelMusicManager
    Signed-off-by: André Gustavo Nakagomi Lopez <andregnl@usp.br>

--- TCC/Assets/Scripts/LevelMusicManager.cs
+++ TCC/Assets/Scripts/LevelMusicManager.cs
@@ -5,11 +5,27 @@ using UnityEngine;
 public class LevelMusicManager : MonoBehaviour
 {
 	[SerializeField] AudioSource audioSource;
+	[SerializeField] float startVolume = 0.0f;
+	[SerializeField] float endVolume = 0.1f;
+	[SerializeField] float step = 0.01f;
+	[SerializeField] float waitTimeStep = 0.001f;
 
 	void OnTriggerEnter2D(Collider2D col)
 	{
 		if (!col.CompareTag("Player")) return;
 
 		audioSource.enabled = true;
+		StartCoroutine(MusicFadeIn());
+	}
+
+	IEnumerator MusicFadeIn()
+	{
+		for (float vol = startVolume; vol <= endVolume; vol += step)
+		{
+			audioSource.volume = vol;
+			yield return new WaitForSeconds(waitTimeStep);
+		}
+
+		yield break;
 	}
 }
